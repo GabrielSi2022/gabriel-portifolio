b4fb12b71cbd38365341046731c4b9a258017ea6
Gabriel Henrique  Bueno <gabrielhenriquesi1996@gmail.com>
2023-07-27 15:09:26 -0300

    commit

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,37 @@
+import Link from "next/link";
+
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiGithubLine,
+  RiLinkedinLine,
+  RiFacebookLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine size={25} />
+      </Link>
+
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine size={25} />
+      </Link>
+
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine size={25} />
+      </Link>
+
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine size={25} />
+      </Link>
+
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinLine size={25} />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
